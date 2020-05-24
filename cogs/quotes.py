e8eecf770a7834a93da6807c204d8531752a48d2
switch263 <switch263@gmail.com>
2020-05-24 14:58:25 -0500

    Add functionality for grabbing a specific quote by ID

--- cogs/quotes.py
+++ cogs/quotes.py
@@ -29,26 +29,45 @@ class Quotes(commands.Cog):
         print("Quotes module has been loaded\n-----")
 
     @commands.command(aliases=['Quote'])
-    async def quote(self, ctx, subcommand: str = "get", quote: str = None):
+    async def quote(self, ctx, subcommand: str = "get", *, quote: str = None):
         """ commands to add and retrieve quotes to/from the db"""
         if subcommand.lower() == 'add':
-            if len(quote.strip()) > 0:
-                dbconn = sqlite3.connect(dbfile)
-                cur = dbconn.cursor()
-                cur.execute("INSERT INTO quotes VALUES (?)", [quote])
-                rowid = cur.lastrowid
-                dbconn.commit()
-                await ctx.send(":microphone: " + "[" + str(rowid) + "] " + quote + " added to database")
-                dbconn.close()
+            if not quote:
+                await ctx.send(":interrobang: You need to give me a quote to add!")
+                pass
+
+            if quote:
+                if len(quote.strip()) > 0:
+                    # add the quote to the database if it's non-zero
+                    dbconn = sqlite3.connect(dbfile)
+                    cur = dbconn.cursor()
+                    cur.execute("INSERT INTO quotes VALUES (?)", [quote])
+                    rowid = cur.lastrowid
+                    dbconn.commit()
+                    await ctx.send(":microphone: " + "[" + str(rowid) + "] " + quote + " added to database")
+                    dbconn.close()
 
         if subcommand.lower() == "get":
-            # assume user wants a random quote back
-            dbconn = sqlite3.connect(dbfile)
-            cur = dbconn.cursor()
-            randquote = cur.execute("SELECT * FROM quotes ORDER BY RANDOM() LIMIT 1;")
-            randquote = randquote.fetchall()[0][0]
-            await ctx.send(randquote)
-            dbconn.close()
+            try:
+                quote = int(quote)
+                # user wants a specific quote ID
+                dbconn = sqlite3.connect(dbfile)
+                cur = dbconn.cursor()
+                randquote = cur.execute("SELECT * FROM quotes WHERE rowid = ?;", [quote])
+                randquote = randquote.fetchall()[0][0]
+                id = quote
+                await ctx.send("[" + str(id) + "] " + randquote)
+                dbconn.close()
+            except:
+                # assume user wants a random quote back
+                dbconn = sqlite3.connect(dbfile)
+                cur = dbconn.cursor()
+                randquote = cur.execute("SELECT * FROM quotes ORDER BY RANDOM() LIMIT 1;")
+                randquote = randquote.fetchall()[0][0]
+                rowid = cur.execute("SELECT rowid FROM quotes WHERE quote like (?)", [randquote])
+                id = rowid.fetchall()[0][0]
+                await ctx.send("[" + str(id) + "] " + randquote)
+                dbconn.close()
 
 
 def setup(bot):
