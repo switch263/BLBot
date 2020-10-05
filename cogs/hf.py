af1865529f90fca339a1559030d833bed288adb1
switch263 <switch263@gmail.com>
2020-10-05 17:55:26 -0500

    Add HF module

---
+++ cogs/hf.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+
+class hf(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("hf module has been loaded\n-----")
+
+    @commands.command(aliases=['hotfuckin','HotFuckin'])
+    async def hf(self, ctx, *, mockstring: str):
+        """ A Special Response for a Special Group Member """
+        await ctx.send("C:\HOTFUCKIN\\")
+
+
+def setup(bot):
+    bot.add_cog(hf(bot))
