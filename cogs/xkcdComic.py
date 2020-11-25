9fd3f3b140219f06957f638514fedb91f73ba014
Zachary Fouts <zach@linux.com>
2020-11-25 14:53:19 -0600

    Implement search functionality

--- cogs/xkcdComic.py
+++ cogs/xkcdComic.py
@@ -14,13 +14,8 @@ class xkcdComic(commands.Cog):
         print("xkcd module has been loaded\n-----")
 
     @commands.command(name='#xkcd')
-    async def getLatestComicNum(self, ctx):
-        """ Returns the latest xkcd comic """
-        await ctx.send(xkcd.getLatestComicNum())
-
-    @commands.command(name='xkcd')
     async def getImageLink(self, ctx, arg="323"):
-        """ Returns image of comic. Try !xkcd [number] """
+        """ Returns image of comic. Try !#xkcd [number] """
         checker = re.search(r"[0-9]+", arg)
         print(checker.group(0))
         if checker:
@@ -30,6 +25,35 @@ class xkcdComic(commands.Cog):
             except:
                 pass
 
+    @commands.command(name='xkcd')
+    async def getImageLink(self, ctx, arg):
+        """ Returns image of comic. Try !xkcd [search string] """
+        headers = {
+            'Connection': 'keep-alive',
+            'Accept': '*/*',
+            'DNT': '1',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Origin': 'https://relevant-xkcd.github.io',
+            'Sec-Fetch-Site': 'cross-site',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Dest': 'empty',
+            'Referer': 'https://relevant-xkcd.github.io/',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'sec-gpc': '1',
+        }
+
+        data = {
+          'search': arg
+        }
+
+        response = requests.post('https://relevant-xkcd-backend.herokuapp.com/search', headers=headers, data=data)
+
+        js = response.json()
+        xkcd_image = js["results"][0]["image"]
+        xkcd_title = js["results"][0]["title"]
+
+        await ctx.send(f"{xkcd_title} {xkcd_image}")
 def setup(bot):
     bot.add_cog(xkcdComic(bot))
 
