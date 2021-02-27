e4a2c8aa31c1fceb9455a612a466874b06cbb5e9
switch263 <switch263@gmail.com>
2021-02-27 16:56:12 -0600

    add lasaga.py

---
+++ cogs/lasaga.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+
+class lasaga(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Lasaga module has been loaded\n-----")
+
+    @commands.command(aliases=['Lasaga'])
+    lasagaimg = 'https://user-images.githubusercontent.com/1498712/101569286-32851600-39a2-11eb-9a0a-e2b1c7b69fc8.png'
+    await ctx.send(lasagaimg)
+
+
+def setup(bot):
+    bot.add_cog(lasaga(bot))
