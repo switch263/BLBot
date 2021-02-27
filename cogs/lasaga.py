d428d43fa74d2c65cce27acc96c96f084617ddc8
switch263 <switch263@gmail.com>
2021-02-27 17:00:01 -0600

    fix missing def

--- cogs/lasaga.py
+++ cogs/lasaga.py
@@ -11,6 +11,7 @@ class lasaga(commands.Cog):
         print("Lasaga module has been loaded\n-----")
 
     @commands.command(aliases=['Lasaga'])
+    async def lasaga(self, ctx):
     lasagaimg = 'https://user-images.githubusercontent.com/1498712/101569286-32851600-39a2-11eb-9a0a-e2b1c7b69fc8.png'
     await ctx.send(lasagaimg)
 
