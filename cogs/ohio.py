aaf0b3f17441551b55abd0a83d252eb4689def51
switch263 <switch263@gmail.com>
2021-07-16 19:00:06 -0500

    add ohio module

--- cogs/ohio.py
+++ cogs/ohio.py
@@ -1,8 +1,6 @@
 import discord
 from discord.ext import commands
 
-kevid='<@361219124979826698>'
-
 class ohio(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -12,8 +10,12 @@ class ohio(commands.Cog):
         print("Ohio module has been loaded\n-----")
 
     @commands.command(aliases=['Ohio'])
-    async def lasaga(self, ctx):
-        await ctx.send("1-800-FUCK-OHIO! " + kevid)
+    async def lasaga(self, ctx, member: discord.Member = None):
+        if member:
+            target = member.mention
+            await ctx.send("1-800-FUCK-OHIO! " + target)
+        else:
+            await ctx.send("1-800-FUCK-OHIO!")
 
 
 def setup(bot):
