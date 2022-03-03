1f3928fc2388d2c0355f01040a950552516ea672
switch263 <switch263@gmail.com>
2022-03-02 22:26:01 -0600

    first pass

---
+++ cogs/tarkov-time.py
@@ -0,0 +1,24 @@
+from discord.ext import commands
+import requests
+
+class tarkov_time(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Tarkov time module has been loaded\n-----")
+
+    @commands.command(aliases=['ttime','tarkovtime'])
+    async def tarkov_time(self, ctx, temp: float):
+        """ fetches current in-game times for Escape from Tarkov"""
+        message = ""
+        timejson = requests.get("https://tarkov-time.adam.id.au/api").json()
+        for side in timejson:
+            message = message + side + ": " + timejson[side] + "\n"
+        await ctx.send(message)
+
+
+def setup(bot):
+    bot.add_cog(tarkov_time(bot))
+
